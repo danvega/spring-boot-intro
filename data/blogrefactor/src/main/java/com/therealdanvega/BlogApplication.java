057ed1f62ba10540c94004ec700cc3239277dc5f
Dan Vega <danvega@gmail.com>
2015-11-03 16:15:10 -0500

    blog refactor demos

---
+++ data/blogrefactor/src/main/java/com/therealdanvega/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.therealdanvega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+	
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+    
+}
