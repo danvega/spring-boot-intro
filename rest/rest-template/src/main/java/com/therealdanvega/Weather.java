37e7501a535e32db830495858e66a48f57e34e75
Dan Vega <danvega@gmail.com>
2015-11-10 12:01:29 -0500

    rest template demo

---
+++ rest/rest-template/src/main/java/com/therealdanvega/Weather.java
@@ -0,0 +1,42 @@
+package com.therealdanvega;
+
+public class Weather {
+	
+	private int temp;
+	private String description;
+	private Location location;
+	
+	public Weather() {
+		super();
+	}
+
+	public int getTemp() {
+		return temp;
+	}
+
+	public void setTemp(int temp) {
+		this.temp = temp;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	@Override
+	public String toString() {
+		return "Weather [temp=" + temp + ", description=" + description + "]";
+	}
+
+}
---
+++ rest/rest-template/src/main/java/com/therealdanvega/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.therealdanvega.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.therealdanvega.Weather;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public Weather home(){
+    	RestTemplate restTemplate = new RestTemplate();
+    	Weather weather = restTemplate.getForObject("http://localhost:8080/data/weather.json", Weather.class);
+    	
+    	return weather;
+	}
+	
+}
