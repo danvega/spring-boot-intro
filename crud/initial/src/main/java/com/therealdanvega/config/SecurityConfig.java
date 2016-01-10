3025c75bed9caf7054c6680d456d27fac88257ce
Dan Vega <danvega@gmail.com>
2016-01-09 19:18:18 -0500

    few changes around the spring security config

--- crud/initial/src/main/java/com/therealdanvega/config/SecurityConfig.java
+++ crud/initial/src/main/java/com/therealdanvega/config/SecurityConfig.java
@@ -16,8 +16,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.anyRequest().permitAll()
 				.antMatchers("/admin/**").hasRole("ADMIN")
+				.anyRequest().permitAll()
 				.and()
 			.formLogin()
 				.loginPage("/login")
